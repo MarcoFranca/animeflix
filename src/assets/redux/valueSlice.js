085b767ef77261d189ba99df9478e5dd4ed230a0
Marco Franca <dev.marcotullio@gmail.com>
2022-10-28 11:33:13 -0300

    add wait value[UPDATE]

--- src/assets/redux/valueSlice.js
+++ src/assets/redux/valueSlice.js
@@ -3,9 +3,9 @@ import {createSlice} from "@reduxjs/toolkit";
 const initialState = {
     animeInfo:{},
     userCategory:'Anime',
-    categoryColor:false,
     anime:false,
     manga:false,
+    wait:false,
 }
 
 export const valueSlice = createSlice({
@@ -18,19 +18,19 @@ export const valueSlice = createSlice({
         setUserCategory: (state, action)=>{
             state.userCategory = action.payload
         },
-        setCategoyColor: (state, action)=>{
-            state.categoryColor = action.payload
-        },
         setAnime: (state, action)=>{
             state.anime = action.payload
         },
         setManga: (state, action)=>{
             state.manga = action.payload
         },
+        setWait: (state, action)=>{
+            state.wait = action.payload
+        },
     }
 
 })
 
-export const {setAnimes, setUserCategory, setCategoyColor, setAnime, setManga} = valueSlice.actions;
+export const {setAnimes, setUserCategory, setAnime, setManga, setWait} = valueSlice.actions;
 
 export default valueSlice.reducer;
