365019b29aec64b9b45795224e7f5386165bc760
Marco Franca <dev.marcotullio@gmail.com>
2022-10-27 22:11:42 -0300

    new values set for changing categories and colors[UPDATE]

--- src/assets/redux/valueSlice.js
+++ src/assets/redux/valueSlice.js
@@ -2,19 +2,35 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     animeInfo:{},
+    userCategory:'Anime',
+    categoryColor:false,
+    anime:false,
+    manga:false,
 }
 
 export const valueSlice = createSlice({
-    name:'values',
+    name:'value',
     initialState,
     reducers:{
         setAnimes: (state, action)=>{
             state.animeInfo = action.payload
-        }
+        },
+        setUserCategory: (state, action)=>{
+            state.userCategory = action.payload
+        },
+        setCategoyColor: (state, action)=>{
+            state.categoryColor = action.payload
+        },
+        setAnime: (state, action)=>{
+            state.anime = action.payload
+        },
+        setManga: (state, action)=>{
+            state.manga = action.payload
+        },
     }
 
 })
 
-export const {setAnimes} = valueSlice.actions;
+export const {setAnimes, setUserCategory, setCategoyColor, setAnime, setManga} = valueSlice.actions;
 
 export default valueSlice.reducer;
