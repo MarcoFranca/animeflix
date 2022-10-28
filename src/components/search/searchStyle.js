10f4e069b62530c694dc0d861fbde1633a6741c9
Marco Franca <dev.marcotullio@gmail.com>
2022-10-27 22:09:57 -0300

    styling search [ADD]

---
+++ src/components/search/searchStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  :hover {
+    box-shadow: rgba(184, 134, 11, 0.5) 0 2px 15px, rgba(184, 134, 11, 0.57) 0 6px 9px;
+  }
+`
