a23f82a9e0ae7228dd708a4fb0cbd5aae8838ff3
Marco Franca <dev.marcotullio@gmail.com>
2022-10-27 22:07:44 -0300

    home page implementation and styling [UPDATE]

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {getAnime} from "../../services/api/req";
+import {useDispatch, useSelector} from "react-redux";
+import animeLoading from "../../assets/image/goku_70.gif"
+import Navigate from "../../components/header/navigate";
+import {Body, Card, CardContent, Cards, Main, Title} from "./homeStyle";
+import {setDisplayText} from "../../assets/redux/apiSlice";
+
 
 export default function Home() {
+    const api = useSelector((state)=> state.api)
+    const animes = useSelector((state)=> state.value.animeInfo.data)
+    const category = useSelector((state)=> state.value.userCategory)
+    const dispatch = useDispatch()
+
+
+    useEffect(()=>{
+        api.text &&
+        dispatch(setDisplayText("")) &&
+        getAnime(api.url, api.category, api.filter, api.text, dispatch)
+    },[api.text, api.category])
+
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <Body>
+            <Navigate/>
+            <Main>
+                <Title>
+                    <h1>{category}</h1>
+                </Title>
+
+                <Cards>
+                    {!api.text && api.displayText && <div className="loading"><img  src={animeLoading} alt="gif loading..."/><h1>Loading...</h1></div>}
+                    {api.text && animes && animes.map((anime)=>
+                            <Card key={anime.id}>
+                                <img src={anime.attributes.posterImage.original} alt={anime.attributes.canonicalTitle}/>
+                                <CardContent>
+                                    <h1>{anime.attributes.canonicalTitle}</h1>
+                                    <p>Rating <span>: {Math.ceil(anime.attributes.averageRating)}</span></p>
+                                </CardContent>
+                            </Card>
+                        )}
+                </Cards>
+            </Main>
+        </Body>
+
+
     )
 }
---
+++ src/pages/home/homeStyle.js
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import {font} from "../../assets/global/font/fonts";
+
+export const Body = styled.div`
+  background-color: #333232;
+  min-height: calc(100vh - 67px);
+
+`
+
+export const Main = styled.main`
+  ${font.darkBlackD};
+  min-height: 100%;
+
+`
+
+export const Title = styled.div`
+  display: flex;
+  ${font.blackDark};
+  justify-content: center;
+  text-shadow: rgba(184, 134, 11, 0.21) 0 2px 15px, rgba(184, 134, 11, 0.34) 0 6px 9px;
+  padding: 20px;
+  font-size: 3rem;
+  letter-spacing: 5px;
+  color: rgba(184, 134, 11);
+`
+
+export const Cards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 25px;
+  padding: 25px;
+
+  .loading{
+    display: flex;
+    flex-direction: column;
+    justify-self: center;
+    align-items: center;
+    width: 100px;
+    color: white;
+    letter-spacing: 2px;
+
+    img{
+      width: 100%
+    }
+  }
+`
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #333232;
+  color: white;
+  border: solid 1px #727070;
+  border-radius: 5px;
+  overflow: hidden;
+
+  :hover {
+    transform: scale(1.1);
+    transition: 300ms;
+    border: solid 2px rgba(184, 134, 11);
+  }
+  img {
+    object-fit: cover;
+    width: 100%;
+  }  
+`
+export const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  text-align: initial;
+  //min-height: 70px;
+  gap: 10px;
+  justify-content: space-evenly;
+  letter-spacing: 0.1rem;
+  font-size: 0.96rem;
+  h1{
+    
+  }
+  span{
+    //font-family: "Open Sans", sans-serif;
+    ${font.blackDark}
+    font-size: 1rem;
+  }
+  
+`
