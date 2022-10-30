e1651a72e8eb10f89e13a4fbee387b1a3df6fce0
Marco Franca <dev.marcotullio@gmail.com>
2022-10-30 13:50:09 -0300

    implementation of functions for details page [Update]

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,7 @@ import React, {useEffect, useRef} from "react";
 import animeLoading from "../../assets/image/several/goku_70.gif"
 import animewaiting from "../../assets/image/several/sonicImpaciente.gif"
 
-import {getAnime, getDetails} from "../../services/api/req";
+import {getAnime, getDetails, getRelationship} from "../../services/api/req";
 import {useDispatch, useSelector} from "react-redux";
 import Navigate from "../../components/header/navigate";
 import {Body, Card, CardContent, Cards, Main, Title} from "./homeStyle";
@@ -12,16 +12,25 @@ import Footer from "../../components/footer/footer";
 import {setWait} from "../../assets/redux/valueSlice";
 import Pagination from "../../components/pagination/pagination";
 import {setLimit} from "../../assets/redux/paginationSlice";
+import {useNavigate} from "react-router-dom";
 
 
 export default function Home() {
 
+    const navigate = useNavigate()
+
 // *******************   Reducers  **************
+
     const dispatch = useDispatch()
 
     //               ***** Api Slice *****
 
     const api = useSelector((state)=> state.api)
+
+    //               ***** Details Slice *****
+
+    const animeDet = useSelector((state)=> state.details.details)
+    const relationship = useSelector((state)=> state.details.relationshipItens)
 
     //               ***** Pagination Slice *****
 
@@ -34,6 +43,7 @@ export default function Home() {
     const category = useSelector((state)=> state.value.userCategory)
     const wait = useSelector((state)=> state.value.wait)
 
+
     const timeoutRef = useRef(null);
 
     const pageLimit = (numPage)=> {
@@ -43,17 +53,34 @@ export default function Home() {
 
 // function to get the information from the api
 
+// ********* search info ************
+
     useEffect(()=>{
         pageLimit(10)
         api.text && dispatch(setDisplayText("")) &&
         getAnime(api.url, api.category, api.filter, api.text, api.pageLimit, api.pageOffset, dispatch)
     },[api.text, api.category, api.pageOffset])
 
+ // ********* loading... *********
+
     useEffect(()=>{
         clearTimeout(timeoutRef.current)
         if (!api.displayText && !api.text){timeoutRef.current = setTimeout(()=> {dispatch(setWait(true))}
             ,10000)}else {dispatch(setWait(false))}
     },[api.displayText, api.text])
+
+//********* details and relationship *********
+
+    useEffect(()=>{
+        animeDet && getRelationship(animeDet.relationships.genres.links.self ,dispatch)
+    },[animeDet])
+
+    useEffect(()=>{
+        relationship.length > 0 && navigate("./detail")
+    },[relationship])
+
+
+
 
     return (
         <Body>
@@ -71,7 +98,9 @@ export default function Home() {
                         <h1>Loading...</h1></div>}
 
                     {api.text && animes && animes.map((anime)=>
-                        <Card key={anime.id} onClick={()=>getDetails(anime.links.self)}>
+                        <Card key={anime.id} onClick={()=>{
+                            getDetails(anime.links.self,dispatch)
+                        }}>
                             <img src={anime.attributes.posterImage.original} alt={anime.attributes.canonicalTitle}/>
                             <CardContent>
                                 <h1>{anime.attributes.canonicalTitle}</h1>
@@ -88,3 +117,15 @@ export default function Home() {
 
     )
 }
+
+// const goDetails = (navigate, details)=>{
+//     dispatch(getMovieDetails(details))
+//     if (!details){
+//         console.log("falso")
+//     }else {
+//         console.log("verdadeiro")
+//         console.log(details)
+//         navigate('/details')
+//
+//     }
+// }
